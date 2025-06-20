6d8b8ecce0b4b0189fd29269022302f11049a6e2
Karinny16 <karinnymedeiros80@gmail.com>
2025-06-20 16:03:01 -0300

    update e delete

--- Anime/catalogo/anime/urls.py
+++ Anime/catalogo/anime/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.lista_animes, name='lista_animes'),
+    path('novo/', views.criar_anime, name='criar_anime'),
+    path('editar/<int:pk>/', views.atualizar_anime, name='atualizar_anime'),
+    path('deletar/<int:pk>/', views.deletar_anime, name='deletar_anime'),
 ]
--- Anime/catalogo/anime/views.py
+++ Anime/catalogo/anime/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Anime
+from .forms import AnimeForm
 
 def lista_animes(request):
     animes = Anime.objects.all()
     return render(request, 'animes/lista.html', {'animes': animes})
+
+def criar_anime(request):  
+    if request.method == 'POST':
+        form = AnimeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_animes')
+    else:
+        form = AnimeForm()        
+    return render(request, 'animes/form_anime.html', {'form': form})
+
+def atualizar_anime(request, pk):
+    anime = get_object_or_404(Anime, pk=pk)
+    if request.method == 'POST':
+        form = AnimeForm(request.POST, request.FILES, instance=anime)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_animes')
+    else:
+        form = AnimeForm(instance=anime)
+    return render(request, 'animes/form_anime.html', {'form': form})
+
+def deletar_anime(request, pk):
+    anime = get_object_or_404(Anime, pk=pk)
+    if request.method == 'POST':
+        anime.delete()
+        return redirect('lista_animes')
+    return render(request, 'animes/confirmar_exclusao.html', {'anime': anime})
